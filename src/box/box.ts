5ab275793d0d1652ea3f3f9ebe3e7d788249bcce
WMXPY <wm@mengw.io>
2018-10-23 11:48:46 -0500

    init packages

---
+++ src/box/box.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace Box
+ * @description Box
+ */
+
+export class Box {
+
+}
---
+++ test/box/box.test.ts
@@ -0,0 +1,15 @@
+/**
+ * @author WMXPY
+ * @namespace Box
+ * @description Box Test
+ */
+
+import { expect } from 'chai';
+
+describe('Placeholder', (): void => {
+
+    it('should be able execute test', async (): Promise<void> => {
+
+        expect(1).to.be.equal(1);
+    });
+});

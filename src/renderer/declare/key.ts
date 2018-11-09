28769f9e334a697bf75c6696c6f8cf15168f36a8
WMXPY <wm@mengw.io>
2018-11-08 23:04:47 -0600

    add autocomplete

---
+++ src/renderer/declare/key.ts
@@ -0,0 +1,11 @@
+/**
+ * @author WMXPY
+ * @namespace Declare
+ * @description Key
+ */
+
+export enum KEY {
+    BACKSPACE = 'Backspace',
+    ESCAPE = 'Escape',
+    TAB = 'Tab',
+}
--- src/renderer/scene/execute/page/box.tsx
+++ src/renderer/scene/execute/page/box.tsx
@@ -4,12 +4,15 @@
  * @description Box
  */
 
+import { ICommand } from '#P/declare';
+import { Provider } from '#P/provider';
 import * as styleExecute from '#R^style/scene/execute/execute.sass';
 import { Panel } from '#R~execute/components/panel';
 import { setCounter } from '#R~execute/state/box/box';
 import { IStore } from '#R~execute/state/declare';
 import * as React from "react";
 import { connect } from "react-redux";
+import { KEY } from '../../../declare/key';
 
 export interface IBoxProps {
 
@@ -83,21 +86,37 @@ export class Box extends React.Component<IBoxProps, IBoxState> {
 
     private _handleKeyDown(event: KeyboardEvent): void {
 
-        if (event.key === 'Escape') {
-            this.setState({
-                current: '',
-            });
+        const provider: Provider = Provider.instance;
+        const current: string = this.state.current;
+
+        const setCurrent = (newCurrent: string) => this.setState({ current: newCurrent });
+
+        switch (event.key) {
+            case KEY.ESCAPE:
+                setCurrent('');
+                break;
+            case KEY.TAB:
+                const nearest: ICommand | null = provider.nearest(current);
+                if (nearest) setCurrent(nearest.command);
+                break;
+            case KEY.BACKSPACE:
+                setCurrent(current.substring(0, current.length - 1));
+                break;
         }
+
         return;
     }
 
     private _handleKeyPress(event: KeyboardEvent): void {
 
-        const current = this.state.current + event.key;
+        if (event.key.length === 1) {
+            const current = this.state.current + event.key;
 
-        this.setState({
-            current,
-        });
+            this.setState({
+                current,
+            });
+        }
+
         return;
     }
 }

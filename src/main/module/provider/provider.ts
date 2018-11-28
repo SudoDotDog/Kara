2f25e09024fec3413049d381bb5bef58896d6352
WMXPY <wm@mengw.io>
2018-11-27 21:24:32 -0600

    add init provider func

--- src/main/module/provider/provider.ts
+++ src/main/module/provider/provider.ts
@@ -4,10 +4,20 @@
  * @description Provider
 */
 
+import { StorageFile } from "#C/storage/file";
+import { Storage } from "#C/storage/storage";
+import { ICommand } from "#P/declare";
+import { MainProvider } from "#P/main";
 import { initMainProvider } from "#P/module/init";
 
 export const bindingMainProvider = (): void => {
 
+    const providerResource: StorageFile<ICommand[]> = Storage.instance.resource('Provider');
+
+    const provider: MainProvider = MainProvider.instance;
+    if (!provider.isEmpty()) {
+        return;
+    }
     initMainProvider();
     return;
 };
